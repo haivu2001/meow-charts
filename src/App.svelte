fa6f1672a5dc7ed2bd0a24247c13e5a10e2a4e70
haivu2001 <29654375+haivu2001@users.noreply.github.com>
2021-04-16 16:26:01 +0700

    modify ColumnChart.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
     import ColumnChart from "./ColumnChart.svelte";
-
-    let items = [54, 55, 97, 55, 87, 30, 44, 11, 22]
 </script>
 
 <main>
-    <ColumnChart items="{items}" gap={5} xOffsets="{[7,0,-7,7,0,-7,7,0,-7]}"
-                 columnColors={ ["red", "orange", "blue", "red", "orange", "blue", "red", "orange", "blue"]}
-                 xLabels={[null,"Jan",null,false,"Feb",false,false,"Mar",false]}/>
-    <ColumnChart items={[345,399,224,200,454,40,477]} fill="darkgreen"
+    <ColumnChart items={[45,61,33,75,11,55,98]}
+                 fill="blue"
                  xLabels={["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]}
-                 max={500}
-                 yLabels={[0,50,100,150,200,250,300,350,400,450,500]} formatter={(x) => `${x}cm`} width="460"/>
+                 yLabels={["0%","25%","50%","75%","100%"]} formatter={(x,y) => `It's ${y}% done as of ${x}.`}
+                 columnColors={["red","orange","pink","purple","yellow","darkred","brown"]}
+                 width="100%" height="100%"/>
 </main>
--- src/ColumnChart.svelte
+++ src/ColumnChart.svelte
@@ -1,81 +1,93 @@
 <script lang="ts">
-    export let width = 400
-    export let height = 240
+    import {fade} from 'svelte/transition';
+
+    export let width = "100%"
+    export let height = "100%"
     export let items: any[]
     export let max = 100
     export let yLabels = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     export let fill = "darkblue"
-    export let gap = 10
-    export let xOffsets = items.map(() => 0)
+    export let gap = 1
     export let columnColors: any[] = []
     export let xLabels: any[] = []
-    export let formatter = x => `${x}%`
-    let x1 = 30 + 0.5
-    let x2 = width - 10 + 0.5
-    const yToBottom = 24
-    let columnHeights = items.map(x => x / max * (height - yToBottom) * (yLabels.length / (yLabels.length + 1)) - 1)
+    export let formatter = (x, y) => `${y}`
+
+    let mouseX = -1
+    let mouseY = -1
+    let mouseItem = 0
+    let mouseTime = 0
+
+    $: columnHeights = items.map(x => x / max * 100)
 </script>
 
-<svg {width} {height}>
-    <!--Grid lines-->
-    <line {x1} {x2} y1="{height-yToBottom+0.5}" y2="{height-yToBottom+0.5}" stroke="black"
-          shape-rendering="crispEdges" stroke-width="0.5"/>
-    <text class="small" x="5" y="{height-yToBottom+0.5-height/yLabels.length*-0.15}">{yLabels[0]}</text>
-    {#each yLabels.slice(1) as item,index}
-        <line {x1} {x2} shape-rendering="crispEdges" stroke-width="0.5"
-              y1="{height-yToBottom-((height-yToBottom)/yLabels.length)*(index+1)+0.5}"
-              y2="{height-yToBottom-((height-yToBottom)/yLabels.length)*(index+1)+0.5}"
-              stroke="black" opacity="0.3"/>
-        <text class="small" x="5" y="{height-yToBottom-((height-yToBottom)/yLabels.length)*(index+1)}"
-              alignment-baseline="central">{item}</text>
-    {/each}
-    <!--Columns-->
-    {#each items as item,index}
-        <g class="column-container">
-            <rect x="{x1+gap+(width-40)/items.length*index+xOffsets[index]}"
-                  y="{height-yToBottom-columnHeights[index]+0.5}"
-                  width="{(width-40)/items.length-gap*2 }" height="{columnHeights[index]}"
-                  fill="{columnColors[index]?columnColors[index]:fill}" opacity="0.7"
-                  class="column"/>
-            <rect x="{x1+gap+(width-40)/items.length*index+xOffsets[index]}"
-                  y="0"
-                  width="{(width-40)/items.length-gap*2 }" height="{height-yToBottom}"
-                  fill="{columnColors[index]?columnColors[index]:fill}" opacity="0.0"
-                  class="litter"/>
-            <foreignObject height="35" width="{(width-40)/items.length-gap*2 }" opacity="0" class="dataDisplay"
-                           x="{x1+gap+(width-40)/items.length*index+xOffsets[index]}"
-                           y="{height-yToBottom-columnHeights[index]-18}">
-                <div class="small" style="padding: 3px">{formatter(item)}</div>
-            </foreignObject>
-            <foreignObject height="35" width="{(width-40)/items.length-gap*2}"
-                           x="{x1+gap+(width-40)/items.length*index+xOffsets[index]}">
-                {#if xLabels[index]}
-                    <div class="small x-label">{xLabels[index]}</div>
-                {/if}
-            </foreignObject>
-        </g>
-    {/each}
+<svg {width} {height} viewBox="0 0 400 240">
+    <svg x="24" y="20" height="220" width="376" viewBox="0 0 {(items.length)*(20*gap)+9} 100"
+         preserveAspectRatio="none">
+        {#each yLabels.reverse() as label,index}
+            <line x1="0" x2="100%" y1={index*100/yLabels.length+0.5} y2={index*100/yLabels.length+0.5} stroke="gray"
+                  shape-rendering="crispEdges" stroke-width="0.1px"/>
+            <text class="label" x="-2" y="{index*100/yLabels.length+0.5}" alignment-baseline="middle"
+                  text-anchor="end">{label}</text>
+        {/each}
+        {#each items as item, index}
+            <g class="column-g" on:mousemove={(x)=>{
+                mouseX = x.clientX
+                mouseY = x.clientY
+                mouseItem = item
+                mouseTime = xLabels[index]
+            }}
+               on:mouseleave={()=> {mouseX = -1;mouseY =-1}}>
+                <rect x={21*index+2.5} y={100-columnHeights[index]-0.5}
+                      width={18} height={columnHeights[index]} opacity="0.7"
+                      fill={columnColors[index]? columnColors[index] : fill} shape-rendering="crispEdges"/>
+                <rect x={21*index+2.5} y=0.5
+                      width=18 height=99 opacity="0.0"
+                      fill={columnColors[index]? columnColors[index] : fill} shape-rendering="crispEdges" class="bulb"/>
+                <svg x={21*index+2.5} y="-8" width=18 height="10" viewBox="0 0 40 40" preserveAspectRatio="none">
+                    <polygon points="0.5,0.5, 39.5,0.5, 39.5,24.5, 22.5,24.5, 20.5,32.5, 18.5,24.5, 0.5,24.5"
+                             stroke="black"
+                             stroke-width="0.3px"
+                             shape-rendering="geometricPrecision"
+                             fill="none" opacity="0" class="bulb"/>
+                </svg>
+            </g>
+        {/each}
+        <line x1="0" x2="100%" y1="99.5" y2="99.5" stroke="black" stroke-width="0.1px" shape-rendering="crispEdges"/>
+    </svg>
+    <svg x="24" y="0" height="20" width="376" viewBox="0 0 {(items.length)*(20*gap)+9} 20"
+         preserveAspectRatio="xMinYMid slice">
+        {#each xLabels as label,index}
+            <text fill="black" alignment-baseline="middle" x={21*index+2.5+9} text-anchor="middle" y=10 class="label">
+                {label}
+            </text>
+        {/each}
+    </svg>
+
 </svg>
+{#if mouseX > 0}
+    <div class="info" transition:fade
+         style="position: fixed;left: {mouseX-32}px;top:{mouseY+30}px">{formatter(mouseTime, mouseItem)}</div>
+{/if}
 
 <style lang="sass">
-  .small
-    font: 10px Lucida, sans-serif
-    text-align: center
+  .column-g:hover > .bulb
+    opacity: 0.2
 
-  .x-label
-    border: 1px solid lightgray
-    padding: 1px
-    border-radius: 10%
-
-  .column-container:hover > foreignObject > .x-label
-    border: 1px solid gray
-
-  .column-container:hover > .column
-    opacity: 0.9
-
-  .column-container:hover > .litter
-    opacity: 0.05
-
-  .column-container:hover > .dataDisplay
+  .column-g:hover > svg > .bulb
     opacity: 1.0
+
+  .label
+    font: 3px Lucida, sans-serif
+
+  svg > svg
+    overflow: visible
+
+  .info
+    width: 120px
+    height: 60px
+    background: white
+    border: 2px solid lightgray
+    border-radius: 0 5px 15px 5px
+    opacity: 50%
+    padding: 5px
 </style>
